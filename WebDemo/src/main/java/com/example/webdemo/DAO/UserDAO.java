1b33ea98907a18d6638d3c32fae48df1844b0c95
YuHong <jack1996yuc@gmail.com>
2023-09-11 21:11:30 +0800

    網頁專案改成spring boot框架，資料庫改為使用Docker的postgres DB，增加註冊帳號功能

--- WebDemo/src/main/java/com/example/webdemo/DAO/UserDAO.java
+++
@@ -1,6 +0,0 @@
-package com.example.webdemo.DAO;
-
-public interface UserDAO {
-    public String getPassword(String userName);
-    public String getSalt(String userName);
-}
--- WebDemo/src/main/java/com/example/webdemo/DBConnection/DBConnection.java
+++
@@ -1,7 +0,0 @@
-package com.example.webdemo.DBConnection;
-
-import java.sql.Connection;
-
-public interface DBConnection {
-    public Connection getConnection();
-}
